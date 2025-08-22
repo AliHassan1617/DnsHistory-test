d98923ef190be244dda1a7acb5c04e4301627c92
AliHassan1617 <alihassan.ah1617@gmail.com>
2025-08-22 12:18:40 +0500

    Replace fe folder with version from Ali/New-FE-Zoros

---
+++ fe/astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [tailwind()],
+  server: {
+    port: 4321
+  },
+  vite: {
+    server: {
+      fs: {
+        allow: ['..']
+      }
+    }
+  }
+});
